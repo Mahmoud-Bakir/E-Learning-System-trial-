4ea0893acb8f7325750b4e1223b12c4314a2bf8d
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-04-02 02:34:49 +0300

    Added Navbar

---
+++ frontend/my-app/src/components/NavBar/nav.jsx
@@ -0,0 +1,18 @@
+import "./nav.css"
+
+const Nav =()=>{
+return(
+    <nav>
+    <div className="nav-links-container">
+        <a href="">Enroll</a>
+        <a href="" >Withdraw</a>
+        <a href="">Documents</a>
+        <a href="">Login</a>
+        <a href="">Register</a>
+       
+    </div>
+</nav>
+)
+
+}
+export default Nav
--- frontend/my-app/src/components/Signup_form/signup.jsx
+++ frontend/my-app/src/components/Signup_form/signup.jsx
@@ -1,11 +1,18 @@
+import Nav from "../NavBar/nav"
 import Partition from "../Partition/partition"
 import "./signup.css"
 const SignupForm=()=>{
-    return(
+    return(<>
+        <Nav/>
         <div className="form_container">
+            <h1>Sign Up</h1>
+            <Partition lname={"First Name"} itype={"text"}/>
+            <Partition lname={"Last Name"} itype={"text"}/>
             <Partition lname={"Email"} itype={"email"}/>
-            <Partition lname={"Email"} itype={"email"}/>
+            <Partition lname={"password"} itype={"password"}/>
+            
         </div>
+        </>
     )
 }
 export default SignupForm
