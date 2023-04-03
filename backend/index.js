0e0cc607ad25d6e2d89c82041e3f60a8eac68b53
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-04-03 09:20:08 +0300

    Debugged buffering error

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const connectDB = require("./configs/db.config");
 app.use(express.json())
 
 const authRouter = require("./routes/auth.routes")
 app.use('/auth', authRouter)
 
-app.listen(process.env.PORT, (err) => {
+app.listen(process.env.PORT, async (err) => {
+  await connectDB();
   if (err) console.error(err)
   console.log("Server is running on port ", process.env.PORT);
   require("./configs/db.config")
 });
+
